26b0072fb5ed2f299a35fc5bbce5b6919c4f2471
sragu2000 <s.s.raguraj@gmail.com>
2022-04-14 23:03:08 +0530

    nav bar chaned

--- application/views/vw_navbar.php
+++ application/views/vw_navbar.php
@@ -10,15 +10,19 @@
         <li class="nav-item">
           <a class="nav-link active" href="<?php echo base_url('module/addarticles');?>"><i class="fas fa-plus"></i>&nbsp;Add Articles</a>
         </li>
-        &nbsp;
+
         <li class="nav-item">
           <a class="nav-link active" href="<?php echo base_url('view/myarticles');?>">
           <i class="fa-solid fa-file-signature"></i></i>&nbsp;My Articles</a>
         </li>
-        &nbsp;
+
         <li class="nav-item">
           <a class="nav-link active" href="<?php echo base_url('dashboard/viewuserdetails');?>">
           <i class="fa-solid fa-gear"></i></i></i>&nbsp;My Account</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="<?php echo base_url('dashboard/logout');?>">
+          <i class="fas fa-power-off"></i> &nbsp;Logout</a>
         </li>
         <!-- <li class="nav-item">
           <a class="nav-link" href="#"><i class="far fa-clipbsoard"></i>&nbsp;Exam</a>
@@ -40,9 +44,7 @@
         <!-- <li class="nav-item">
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li> -->
-        <li class="nav-item">
-          <a class="nav-link active" href="<?php echo base_url('dashboard/logout');?>"><i class="fas fa-power-off"></i> &nbsp;Logout</a>
-        </li>
+        
       </ul>
       <!-- <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
